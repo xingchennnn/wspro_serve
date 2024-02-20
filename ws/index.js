11a43bb93973fa37e9609931d32355c93c8a5c0c
饶奎 <2955095425@qq.com>
2024-02-20 10:27:39 +0800

    新增历史消息

--- ws/index.js
+++ ws/index.js
@@ -7,11 +7,18 @@ const wss = new WebSocket.Server({ server });
    
 const clients = new Set();
 
+const messageList = []
    
 wss.on('connection', (ws , req) => {
   const myURL = new URL(req.url, `http://${req.headers.host}`);
   let name = myURL.searchParams.get('name')
-  clients.add(ws);  //将新进入的加入里面
+  if(messageList.length!==0){
+    // 给这个人发送历史消息
+
+    ws.send(JSON.stringify(messageList));
+  }
+
+  clients.add(ws);  //将新进入的加入房间列表里面
 
   clients.forEach((client) => {  //新进入的会给所有人发消息
     if (client.readyState === WebSocket.OPEN) {
@@ -22,10 +29,12 @@ wss.on('connection', (ws , req) => {
   
   ws.on('message', (message) => {
     console.log(`消息: ${message}`);
-
-    let data = message.toString()
-
-    console.log(data)
+    //添加进历史消息
+    messageList.push(JSON.parse(message))
+    let str = []
+    str.push(message)
+    let data = str.toString()
+    // console.log('data' , data)
     clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data);
@@ -38,7 +47,6 @@ wss.on('connection', (ws , req) => {
     console.log('客户端连接断开');
     
     clients.delete(ws);
-    // console.log(clients)
   });
 });
 
